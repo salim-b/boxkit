70da016f0743cac3300953d00a26dcfbd6eb32b2
Salim B <git@salim.space>
2025-06-19 13:48:00 +0200

    refactor: don't run R init on every new session

--- scripts/runtime/user_config.sh
+++ scripts/runtime/user_config.sh
@@ -3,9 +3,3 @@
 # env vars to be set at user session start
 export R_LIBS_USER="$HOME/.local/lib/r/%p-library/%v"
 
-# run TinyTeX and R install/update scripts
-if [ -z "$R_INIT_DONE" ]; then
-    export R_INIT_DONE=1
-    install_tinytex.R
-    install_rpkgs.R
-fi
