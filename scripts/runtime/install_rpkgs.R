c8e332145e25b57c423933497e80c49a959e2a27
Salim B <git@salim.space>
2025-07-29 16:24:50 +0200

    refactor: switch to upstream quarto R pkg

--- scripts/runtime/install_rpkgs.R
+++ scripts/runtime/install_rpkgs.R
@@ -121,7 +121,7 @@ pkgs_cran <- c(
   "purrr",
   "qpdf",
   "qs",
-#  "quarto", # dev version in use
+  "quarto"
   "R6",
   "ragg",
   "rcmdcheck",
@@ -233,7 +233,6 @@ remotes::install_gitlab(repo = "zdaarau/rpkgs/rdb@pg",
 ## third-party pkgs with notable (own) unmerged PRs for which we maintain an "interim" branch
 pak::pkg_install(pkg = c("github::salim-b/lintr", # TODO: switch to CRAN version once lintr 3.2.1+ is released
                          "github::salim-b/plotly.R@interim-merge",
-                         "github::salim-b/quarto-r", # TODO: switch to CRAN version once quarto 1.4.5+ is released
                          "github::salim-b/roxygen2@interim-merge", # switch to `r-lib/roxygen2` once https://github.com/r-lib/roxygen2/pull/1712 is merged
                          "github::salim-b/rprojroot@interim-merge",
                          "github::salim-b/rscodeio@interim-merge"),
