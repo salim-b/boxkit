8ce50ed83aa7ca70b7b032146e152336bcc67d77
Salim B <git@salim.space>
2025-08-03 00:32:56 +0200

    refactor: extend PATH at user session start

--- scripts/runtime/user_config.sh
+++ scripts/runtime/user_config.sh
@@ -1,5 +1,6 @@
 # shellcheck disable=SC2148
 
 # env vars to be set at user session start
-export R_LIBS_USER="$HOME/.local/lib/r/%p-library/%v"
+export R_LIBS_USER="${HOME}/.local/lib/r/%p-library/%v"
+export PATH="${HOME}/.local/bin:${PATH}"
 
