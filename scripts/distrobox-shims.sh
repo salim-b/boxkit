922afdedae2c2b1ed67957bb95ef3ffc131a1f8b
Salim B <git@salim.space>
2025-08-13 14:36:10 +0200

    fix: add shim for `xdg-open`
    (Distrobox's built-in `xdg-open` shim creation stopped working for some reason)

--- scripts/distrobox-shims.sh
+++ scripts/distrobox-shims.sh
@@ -5,3 +5,4 @@ ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/docker
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/podman
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/rpm-ostree
+ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/xdg-open
