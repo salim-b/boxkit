be2812e9951375e699313c64d0cb52b721b0ac41
Salim B <git@salim.space>
2026-02-05 15:44:56 +0100

    refactor: add shims for `container-use`

--- scripts/distrobox-shims.sh
+++ scripts/distrobox-shims.sh
@@ -6,3 +6,7 @@ ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/podman
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/rpm-ostree
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/xdg-open
+
+# tools installed via Homebrew on the host (inside the container, `(/var)/home/homebrew` is unavailable)
+ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/cu
+ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/container-use
