7406aa13ae0fa9e044a864279c15c7e666d1c055
Salim B <git@salim.space>
2026-02-08 16:25:29 +0100

    refactor: make sure container-internal executables have precendence

--- scripts/runtime/user_config.sh
+++ scripts/runtime/user_config.sh
@@ -2,5 +2,6 @@
 
 # env vars to be set at user session start
 export R_LIBS_USER="${HOME}/.local/lib/r/%p-library/%v"
-export PATH="${HOME}/.local/bin:${PATH}"
+## make sure container-internal executables have precendence over the host's executables
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:${PATH}"
 
